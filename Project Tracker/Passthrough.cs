4ff394955bb527cd3bd522f5e5d94f84bbda851c
CyanCoding <CyanCoding@users.noreply.github.com>
2020-04-29 14:37:45 -0400

    Add project files.

---
+++ Project Tracker/Passthrough.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Tracker {
+	class Passthrough {
+		// We use this to tell EditProgram what file MainWindow is editing when the user
+		// Presses the edit button
+		public static string EditingFile { get; set; }
+	}
+}
