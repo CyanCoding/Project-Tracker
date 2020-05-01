7598d43ef15938561947322be31bedb1af0efa6a
CyanCoding <CyanCoding@users.noreply.github.com>
2020-05-01 13:47:38 -0400

    Update EditProgram modules

--- app/Project Tracker/Project Tracker/EditProgram.xaml.cs
+++ app/Project Tracker/Project Tracker/EditProgram.xaml.cs
@@ -38,6 +38,8 @@ namespace Project_Tracker {
 		Thread timerThread;
 		long lastSecond = 0;
 		long currentSecond = 0;
+		bool isTablesGenerated = false;
+		int rowSelectionID = 0; // We use this to identify which row is currently selected
 
 		Stopwatch stopwatch;
 
@@ -45,6 +47,7 @@ namespace Project_Tracker {
 		readonly Color sortColor = Color.FromRgb(228, 233, 235);
 		readonly Color greenStopwatch = Color.FromRgb(42, 112, 35);
 		readonly Color redStopwatch = Color.FromRgb(156, 9, 9);
+		readonly Color selectionColor = Color.FromRgb(84, 207, 255);
 		readonly FontFamily textFont = new FontFamily("Microsoft Sans Serif");
 
 		public EditProgram() {
@@ -89,6 +92,54 @@ namespace Project_Tracker {
 			newRow.FontSize = 16;
 			newRow.FontFamily = textFont;
 			newRow.Cells.Add(new TableCell(new Paragraph(new Run(value))));
+		}
+
+		private void SelectionChange(bool isUp, Table table, int rowsAdded) {
+			this.Dispatcher.Invoke(() => {
+				if (rowSelectionID < 0) {
+					rowSelectionID = 0;
+				}
+				if (isUp == true && rowsAdded > 1) { // REMEMBER: Going "up" actually brings the selection ID lower.
+					TableRow selectedRow = table.RowGroups[0].Rows[rowSelectionID];
+					selectedRow.Background = new SolidColorBrush(selectionColor);
+
+					if (rowSelectionID % 2 == 0) {
+						TableRow previouslySelectedRow = table.RowGroups[0].Rows[rowSelectionID + 1];
+						previouslySelectedRow.Background = new SolidColorBrush(sortColor);
+					}
+					else {
+						TableRow previouslySelectedRow = table.RowGroups[0].Rows[rowSelectionID + 1];
+						previouslySelectedRow.Background = Brushes.White;
+					}
+
+				}
+				else if (isUp == false) {
+					if (rowSelectionID < rowsAdded) {
+						TableRow selectedRow = table.RowGroups[0].Rows[rowSelectionID];
+						selectedRow.Background = new SolidColorBrush(selectionColor);
+
+						if (rowSelectionID != 0) {
+							if (rowSelectionID % 2 == 0) {
+								TableRow previouslySelectedRow = table.RowGroups[0].Rows[rowSelectionID - 1];
+								previouslySelectedRow.Background = new SolidColorBrush(sortColor);
+							}
+							else {
+								TableRow previouslySelectedRow = table.RowGroups[0].Rows[rowSelectionID - 1];
+								previouslySelectedRow.Background = Brushes.White;
+							}
+						}
+					}
+					else {
+						rowSelectionID--;
+					}
+				}
+			});
+		}
+
+		private void ResetSelection(Table table) {
+			TableRow selectedRow = table.RowGroups[0].Rows[0];
+			selectedRow.Background = new SolidColorBrush(selectionColor);
+			rowSelectionID = 0;
 		}
 
 		private void stopwatchTimer() {
@@ -223,19 +274,6 @@ namespace Project_Tracker {
 				duration = values.duration;
 				percentComplete = values.percent;
 
-				// Calculate table pos
-				int individualSize = ((int)this.Width - 40) / 3;
-				int commentDistance = (int)this.Width - individualSize - 20;
-				int featureDistance = (int)this.Width - (individualSize + individualSize + 40);
-
-				// Set pos of tables and their labels
-				commentScrollView.Margin = new Thickness(commentDistance, 140, 10, 60);
-				featureScrollView.Margin = new Thickness(featureDistance, 140, individualSize + 10, 60);
-				errorScrollView.Margin = new Thickness(10, 140, individualSize + individualSize + 30, 60);
-				errorLabel.Margin = new Thickness(10, 100, 0, 0);
-				featureLabel.Margin = new Thickness(featureDistance, 100, 0, 0);
-				commentLabel.Margin = new Thickness(commentDistance, 100, 0, 0);
-
 				// Add values to tables
 				foreach (string error in values.errors) {
 					AddRow(errorTable, 0, error);
@@ -246,6 +284,14 @@ namespace Project_Tracker {
 				foreach (string comment in values.comments) {
 					AddRow(commentTable, 2, comment);
 				}
+				ResetSelection(errorTable);
+				ResetSelection(featureTable);
+				ResetSelection(commentTable);
+
+				errorScrollView.Focus();
+
+				isTablesGenerated = true;
+
 				this.Title = values.title + " - " + values.percent + "%";
 				durationLabel.Text = values.duration;
 
@@ -312,7 +358,25 @@ namespace Project_Tracker {
 		}
 
 		private void Edit(object sender, MouseButtonEventArgs e) {
+			// Hide everything
+			durationLabel.Visibility = Visibility.Hidden;
+			commentScrollView.Visibility = Visibility.Hidden;
+			errorScrollView.Visibility = Visibility.Hidden;
+			featureScrollView.Visibility = Visibility.Hidden;
 
+			// Create new checkboxes
+			System.Windows.Controls.CheckBox box;
+			StackPanel panel = new StackPanel { Orientation = System.Windows.Controls.Orientation.Vertical };
+			for (int i = 0; i < errors.Length; i++) {
+				box = new System.Windows.Controls.CheckBox();
+				box.Content = errors[i];
+				box.VerticalAlignment = VerticalAlignment.Top;
+				box.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+				box.Margin = new Thickness(10, i * 50, 50, 50);
+
+				panel.Children.Add(box);
+			}
+			this.Content = panel;
 		}
 
 		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
@@ -320,41 +384,53 @@ namespace Project_Tracker {
 			StringBuilder sb = new StringBuilder();
 			StringWriter sw = new StringWriter(sb);
 
-			using (JsonWriter js = new JsonTextWriter(sw)) {
-				js.Formatting = Formatting.Indented;
 
-				js.WriteStartObject();
+			// Make 10 attempts to save the file.
+			for (int i = 0; i < 10; i++) {
+				try {
+					using (JsonWriter js = new JsonTextWriter(sw)) {
+						js.Formatting = Formatting.Indented;
 
-				js.WritePropertyName("Title");
-				js.WriteValue(projectTitle);
-				js.WritePropertyName("Errors");
-				js.WriteStartArray();
-				foreach (string error in errors) {
-					js.WriteValue(error);
-				}
-				js.WriteEnd();
-				js.WritePropertyName("Features");
-				js.WriteStartArray();
-				foreach (string feature in features) {
-					js.WriteValue(feature);
-				}
-				js.WriteEnd();
-				js.WritePropertyName("Comments");
-				js.WriteStartArray();
-				foreach (string comment in comments) {
-					js.WriteValue(comment);
-				}
-				js.WriteEnd();
-				js.WritePropertyName("Duration");
-				js.WriteValue(duration);
-				js.WritePropertyName("Percent");
-				js.WriteValue(percentComplete);
+						js.WriteStartObject();
 
-				js.WriteEndObject();
+						js.WritePropertyName("Title");
+						js.WriteValue(projectTitle);
+						js.WritePropertyName("Errors");
+						js.WriteStartArray();
+						foreach (string error in errors) {
+							js.WriteValue(error);
+						}
+						js.WriteEnd();
+						js.WritePropertyName("Features");
+						js.WriteStartArray();
+						foreach (string feature in features) {
+							js.WriteValue(feature);
+						}
+						js.WriteEnd();
+						js.WritePropertyName("Comments");
+						js.WriteStartArray();
+						foreach (string comment in comments) {
+							js.WriteValue(comment);
+						}
+						js.WriteEnd();
+						js.WritePropertyName("Duration");
+						js.WriteValue(duration);
+						js.WritePropertyName("Percent");
+						js.WriteValue(percentComplete);
+
+						js.WriteEndObject();
+					}
+					using (StreamWriter writer = File.CreateText(editingFile)) {
+						writer.WriteLine(sb.ToString());
+					}
+					break;
+				}
+				catch (IOException) {
+					Thread.Sleep(1000);
+				}
 			}
-			using (StreamWriter writer = File.CreateText(editingFile)) {
-				writer.WriteLine(sb.ToString());
-			}
+
+
 
 			MainWindow main = new MainWindow();
 
@@ -365,6 +441,88 @@ namespace Project_Tracker {
 			if (timerThread.IsAlive) {
 				timerThread.Abort();
 			}
+		}
+
+		private void KeyPress(object sender, System.Windows.Input.KeyEventArgs e) {
+			// Remember that combo box values can be changed by the arrow keys.
+			// We don't want to change the table selection when we're doing this.
+			if (!errorSelection.IsFocused && !featureSelection.IsFocused && !commentSelection.IsFocused) {
+				if (e.Key == Key.Down) {
+					rowSelectionID++;
+
+					if (switchLabels.SelectedIndex == 0) { // Error table is selected
+						SelectionChange(false, errorTable, errorRowsAdded);
+					}
+					else if (switchLabels.SelectedIndex == 1) { // Feature table is selected
+						SelectionChange(false, featureTable, featureRowsAdded);
+					}
+					else { // Comment table is selected
+						SelectionChange(false, commentTable, commentsRowsAdded);
+					}
+
+				}
+				else if (e.Key == Key.Up) {
+					if (rowSelectionID > 0) {
+						rowSelectionID--;
+					}
+
+					if (switchLabels.SelectedIndex == 0) { // Error table is selected
+						SelectionChange(true, errorTable, errorRowsAdded);
+					}
+					else if (switchLabels.SelectedIndex == 1) { // Feature table is selected
+						SelectionChange(true, featureTable, featureRowsAdded);
+					}
+					else { // Comment table is selected
+						SelectionChange(true, commentTable, commentsRowsAdded);
+					}
+				}
+			}
+		}
+
+		private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			// The selection "changes" (is automatically set) when the window
+			// is first opened, but if we try to make changes to one of the
+			// elements that hasn't been initialized or added rows to it yet
+			// then it'll obviously return an error. So we wait until AFTER
+			// the tables have been initialized, aka after startup.
+			if (isTablesGenerated) {
+				ResetSelection(errorTable);
+				ResetSelection(featureTable);
+				ResetSelection(commentTable);
+
+
+				if (switchLabels.SelectedIndex == 0) { // Errors
+					errorScrollView.Visibility = Visibility.Visible;
+					featureScrollView.Visibility = Visibility.Hidden;
+					commentScrollView.Visibility = Visibility.Hidden;
+
+					errorScrollView.Focus();
+				}
+				else if (switchLabels.SelectedIndex == 1) { // Features
+					errorScrollView.Visibility = Visibility.Hidden;
+					featureScrollView.Visibility = Visibility.Visible;
+					commentScrollView.Visibility = Visibility.Hidden;
+
+					featureScrollView.Focus();
+				}
+				else { // Comments
+					errorScrollView.Visibility = Visibility.Hidden;
+					featureScrollView.Visibility = Visibility.Hidden;
+					commentScrollView.Visibility = Visibility.Visible;
+
+					commentScrollView.Focus();
+				}
+			}
+		}
+
+		private void RemoveValue(object sender, MouseButtonEventArgs e) {
+
+		}
+		private void AddValue(object sender, MouseButtonEventArgs e) {
+
+		}
+		private void CheckOff(object sender, MouseButtonEventArgs e) {
+
 		}
 	}
 }
