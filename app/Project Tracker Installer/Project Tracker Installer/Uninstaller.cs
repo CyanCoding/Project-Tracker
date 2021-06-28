d56ab89b14d938dc747d8948822bf4634587706e
Camden Mac Leod <camden.macleod@pm.me>
2021-06-28 17:22:31 -0400

    Added logging and full directory deletion (NOT COMPLETE)

--- app/Project Tracker Installer/Project Tracker Installer/Uninstaller.cs
+++ app/Project Tracker Installer/Project Tracker Installer/Uninstaller.cs
@@ -5,6 +5,12 @@ using System.Windows.Forms;
 
 namespace Project_Tracker_Installer {
 	class Uninstaller {
+
+        private void LogData(string data) {
+            string path = "install-log.txt";
+            File.AppendAllText(path, data + "\n");
+        }
+
         /// <summary>
         /// Main uninstall function.
         /// Prompts user to delete data and deletes every other file.
@@ -14,41 +20,48 @@ namespace Project_Tracker_Installer {
         /// <param name="INSTALL_DIRECTORY">The path of the install program directory.</param>
         /// <param name="REGISTRY">The title of the registry entry to remove.</param>
         /// <param name="SHORTCUT_LOCATION">The path of the shortcut to delete.</param>
-		public void Uninstall(string DATA_DIRECTORY_PATH, string INSTALLER_PATH, string BASE_DIRECTORY, string INSTALL_DIRECTORY, string REGISTRY = "", string SHORTCUT_LOCATION = "") {
-            if (Directory.Exists(DATA_DIRECTORY_PATH)) {
-                var deleteData = MessageBox.Show("Do you wish to delete your projects data?", "Project Tracker Installer", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+		public void Uninstall(string DATA_DIRECTORY_PATH, string INSTALLER_PATH, string BASE_DIRECTORY, string LOG_PATH, string REGISTRY = "", string SHORTCUT_LOCATION = "") {
+            try {
+                LogData("[Uninstaller]: Running...");
+                if (Directory.Exists(DATA_DIRECTORY_PATH)) {
+                    LogData("[Uninstaller]: Asking user if they want to delete project data");
+                    var deleteData = MessageBox.Show("Do you wish to delete your projects data?", "Project Tracker Installer", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
-                if (deleteData == DialogResult.Yes) {
-                    try {
-                        Directory.Delete(DATA_DIRECTORY_PATH, true);
+                    if (deleteData == DialogResult.Yes) {
+                        LogData("[Uninstaller]: Deleting project data in " + DATA_DIRECTORY_PATH);
+                        try {
+                            Directory.Delete(DATA_DIRECTORY_PATH, true);
+                        }
+                        catch (Exception e) { // Probably couldn't delete the directory for some reason
+                            LogData("[Uninstaller ERROR]: Error while deleting directory data: " + e);
+                        }
                     }
-                    catch (Exception) { // Probably couldn't delete the directory for some reason
+                }
 
+                DirectoryInfo di = new DirectoryInfo(BASE_DIRECTORY);
+
+                foreach (FileInfo file in di.GetFiles()) {
+                    string hi = file.FullName; // TODO: This doesn't match either because file.FullName returns a different appdata folder than the on we're using.
+                    if (file.FullName != INSTALLER_PATH && file.FullName != LOG_PATH) {
+                        file.Delete();
+                        LogData("[Uninstaller]: Deleting file " + file);
                     }
                 }
-            }
-            // Removes all files except for in installer path and data path
-            foreach (string f in Directory.GetFiles(BASE_DIRECTORY)) {
-                if (f == INSTALL_DIRECTORY || f == DATA_DIRECTORY_PATH) {
-                    continue;
+                foreach (DirectoryInfo dir in di.GetDirectories()) {
+                    if (dir.FullName != DATA_DIRECTORY_PATH) {
+                        dir.Delete(true);
+                        LogData("[Uninstaller]: Deleting directory " + dir);
+                    }
                 }
 
-                File.Delete(f);
-            }
-
-            // Removes all files except installer in installer path
-            foreach (string f in Directory.GetFiles(INSTALL_DIRECTORY)) {
-                if (f == INSTALLER_PATH) {
-                    continue;
+                if (REGISTRY != "") {
+                    LogData("[Uninstaller]: Registry is not blank, so we delete it");
+                    RemoveRegistry(REGISTRY, SHORTCUT_LOCATION);
                 }
-
-                File.Delete(f);
             }
-
-            if (REGISTRY != "") {
-                RemoveRegistry(REGISTRY, SHORTCUT_LOCATION);
+            catch (Exception e) {
+                LogData("[Uninstaller ERROR]: " + e);
             }
-
         }
 
         /// <summary>
@@ -57,8 +70,13 @@ namespace Project_Tracker_Installer {
         /// <param name="REGISTRY">The title of the registry entry to remove.</param>
         /// <param name="SHORTCUT_LOCATION">The path of the shortcut to delete.</param>
         private void RemoveRegistry(string REGISTRY, string SHORTCUT_LOCATION) {
-            Registry.CurrentUser.DeleteSubKey(REGISTRY);
-            File.Delete(SHORTCUT_LOCATION);
+            try {
+                Registry.CurrentUser.DeleteSubKey(REGISTRY);
+                File.Delete(SHORTCUT_LOCATION);
+            }
+            catch (Exception e) {
+                LogData("[Uninstaller(Registry) ERROR]: " + e);
+            }
         }
 	}
 }
