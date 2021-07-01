dbc83650bc00a1e8386081e6e92cc166cf25d18e
Camden Mac Leod <camden.macleod@pm.me>
2021-06-30 23:53:29 -0400

    Change global variables to public

--- app/Project Tracker/Project Tracker/Resources/Globals.cs
+++ app/Project Tracker/Project Tracker/Resources/Globals.cs
@@ -15,32 +15,32 @@ namespace Project_Tracker.Resources {
         public static readonly string ENCRYPTION_GUID = "FCABA3D4-E5A6-4F14-A41B-4DEEF5AA60B2";
 
         // The file extension for project tracker files
-        private readonly string projectDataExtension = "*.pt";
+        public static readonly string projectDataExtension = "*.pt";
 
         // Generic public color values
-        private readonly Color itemColor = Color.FromRgb(60, 60, 60);
-        private readonly Color labelTextColor = Color.FromRgb(255, 255, 255);
+        public static readonly Color itemColor = Color.FromRgb(60, 60, 60);
+        public static readonly Color labelTextColor = Color.FromRgb(255, 255, 255);
 
         #region Version
-        private readonly bool IS_BETA = false;
+        public static readonly bool IS_BETA = false;
 
         // IF YOU CHANGE THIS, ALSO CHANGE IT IN UpdateWindow.xaml.cs
-        private readonly string CURRENT_VERSION = "2.4";
+        public static readonly string CURRENT_VERSION = "2.4";
 
         // The next version info
-        private readonly string NEXT_VERSION_INFO = Environment.GetFolderPath
+        public static readonly string NEXT_VERSION_INFO = Environment.GetFolderPath
             (Environment.SpecialFolder.LocalApplicationData) + "/Project Tracker/next-version.ptsd";
 
         // The current version info
-        private readonly string VERSION_INFO = Environment.GetFolderPath
+        public static readonly string VERSION_INFO = Environment.GetFolderPath
             (Environment.SpecialFolder.LocalApplicationData) + "/Project Tracker/version.ptsd";
 
         // The URL for downloading the current version
-        private readonly string VERSION_MANIFEST_URL =
+        public static readonly string VERSION_MANIFEST_URL =
             "https://raw.githubusercontent.com/CyanCoding/Project-Tracker/master/install-resources/version-info/version.json";
 
         // The URL for downloading the next version
-        private readonly string NEXT_VERSION_MANIFEST_URL =
+        public static readonly string NEXT_VERSION_MANIFEST_URL =
             "https://raw.githubusercontent.com/CyanCoding/Project-Tracker/master/install-resources/version-info/next-version.json";
 
         #endregion
@@ -48,15 +48,15 @@ namespace Project_Tracker.Resources {
         #region Directories/paths
 
         #endregion
-        private readonly string APPDATA_DIRECTORY =
+        public static readonly string APPDATA_DIRECTORY =
             Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
             + "/Project Tracker";        
 
-        private readonly string DATA_DIRECTORY =
+        public static readonly string DATA_DIRECTORY =
             Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
             + "/Project Tracker/data";
 
-        private readonly string SETTINGS_FILE =
+        public static readonly string SETTINGS_FILE =
             Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
             + "/Project Tracker/settings.ptsd";
     }
