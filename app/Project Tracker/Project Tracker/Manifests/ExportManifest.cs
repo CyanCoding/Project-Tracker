811b5ad2f6f20effb10c0f1fe4913d7238981671
Camden Mac Leod <camden.macleod@pm.me>
2021-07-03 08:25:36 -0400

    Add ExportManifest

---
+++ app/Project Tracker/Project Tracker/Manifests/ExportManifest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Tracker.Manifests {
+    class ExportManifest {
+        public class Rootobject {
+            public string Version { get; set; }
+            public Settings Settings { get; set; }
+            public Project[] Projects { get; set; }
+        }
+
+        public class Settings {
+            public int LastSelectedIndex { get; set; }
+            public bool DisplayingCompleted { get; set; }
+            public bool ForceClose { get; set; }
+        }
+
+        public class Project {
+            public string Title { get; set; }
+            public string[] Tasks { get; set; }
+            public string[] TaskData { get; set; }
+            public string[] TaskIdentifier { get; set; }
+            public object[] LinesOfCodeFiles { get; set; }
+            public string FolderLocation { get; set; }
+            public string Duration { get; set; }
+            public string DateCreated { get; set; }
+            public int TasksMade { get; set; }
+            public int TasksCompleted { get; set; }
+            public string Icon { get; set; }
+            public string Percent { get; set; }
+        }
+
+    }
+}
